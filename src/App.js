b53834d865b9d598689010bb15ca9d956bd92da5
BadriVamsiKrish <www.1203badrivamsi@.com>
2023-08-21 16:49:31 +0530

    task 20/26 ecom web

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter,Routes, Route,useNavigate,Redi } from 'react-router-dom';
+import { useContext } from 'react';
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
-
+import AuthContext from './store/Auth-context';
 function App() {
+  const authCtx=useContext(AuthContext);
+  const history=useNavigate();
+  const fun = () =>{
+    history('/');
+  } 
+
   return (
     <Layout>
       {/* <BrowserRouter> */}
         <Routes>
           <Route exact path='/' element={<HomePage/>}/>
-          <Route path='/auth' element={<AuthPage />}/>
-          <Route path='/profile'element={<UserProfile />}/>
+          {!authCtx.isLoggedIn &&<Route path='/auth' element={<AuthPage />}/>}
+          {authCtx.isLoggedIn &&<Route path='/profile'element={<UserProfile />}/>}
+          {!authCtx.isLoggedIn &&<Route path='/profile'element={<AuthPage/>}/>}
+          <Route path='*'element={<HomePage/>}/>
+
         </Routes>
       {/* </BrowserRouter> */}
     </Layout>
